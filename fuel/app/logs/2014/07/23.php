0c69ba6cecb7d552e9643a702514b9e7473b9ba4
SAKAMOTO <gekkoukisi@gmail.com>
2014-07-23 23:40:45 +0900

    first

---
+++ fuel/app/logs/2014/07/23.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2014-07-23 23:32:58 --> Error - The requested view could not be found: welcome/index in /www/restaurant.planningdev.com/fuel/core/classes/view.php on line 388
+ERROR - 2014-07-23 23:33:28 --> Warning - mkdir(): Permission denied in /www/restaurant.planningdev.com/fuel/vendor/smarty/smarty/distribution/libs/sysplugins/smarty_internal_write_file.php on line 37
